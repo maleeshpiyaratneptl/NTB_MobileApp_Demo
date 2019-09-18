a61c6f9e1c0067d84ac46988a73b7171a0c5963f
maleeshpiyaratneptl <maleeshpiyaratne@pragmatictesters.com>
2019-09-18 13:23:27 +0530

    Update project fixes

--- app/src/test/java/com/ptl/automation/ntb_mobileapp_demo/TestScripts/LoginTestCases.java
+++ app/src/test/java/com/ptl/automation/ntb_mobileapp_demo/TestScripts/LoginTestCases.java
@@ -1,22 +1,27 @@
 package com.ptl.automation.ntb_mobileapp_demo.TestScripts;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
 
 public class LoginTestCases {
 
     AppiumDriver<MobileElement> appiumDriver;
-    DesiredCapabilities capabilities;
 
+    DesiredCapabilities capabilities;
+    WebDriver driver;
     @BeforeTest
     public void setUp() throws MalformedURLException {
 
@@ -25,7 +30,7 @@ public class LoginTestCases {
         capabilities = new DesiredCapabilities();
 
         /* Set android deviceName desired capability. Set your device name. */
-        capabilities.setCapability("deviceName", "J5");
+        capabilities.setCapability("deviceName", "Android SDK built for x86");
 
         /* Set Device ID of your mobile phone */
         capabilities.setCapability("udid", "emulator-5554");
@@ -38,21 +43,30 @@ public class LoginTestCases {
         /*
          * Set android VERSION desired capability. Set your mobile device's OS version.
          */
-        capabilities.setCapability("platformVersion", "7.1.1");
+        capabilities.setCapability("platformVersion", "9");
 
         /*
          * Set android appPackage desired capability. It is com.android.calculator2 for
          * calculator application. Set your application's appPackage if you are using
          * any other app.
          */
-        capabilities.setCapability("appPackage", "net.omobio.dialogsc");
+        capabilities.setCapability("appPackage", "com.android.vending");
 
         /*
          * Set android appActivity desired capability. It is
          * com.android.calculator2.Calculator for calculator application. Set your
          * application's appPackage if you are using any other app.
          */
-        capabilities.setCapability("appActivity", "net.omobio.dialogsc.MainActivity");
+        capabilities.setCapability("appActivity", "com.android.vending.AssetBrowserActivity");
+
+        // Created object of RemoteWebDriver will all set capabilities.
+        // Set appium server address and port number in URL string.
+        // It will launch calculator app in android device.
+//      driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+//        appiumDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+        appiumDriver = new AndroidDriver<MobileElement>(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
+        appiumDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
     }
 
@@ -60,12 +74,33 @@ public class LoginTestCases {
     public void testFirstCalculator() {
 
 
-        // Click on DELETE/CLR button to clear result text box before running test.
-        appiumDriver.findElements(By.xpath("//android.widget.Button")).get(0).click();
+        //Instantiate AppiumDriver
+//        try {
+//            appiumDriver = new AndroidDriver<MobileElement>(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
+//        } catch (MalformedURLException e) {
+//            System.out.println(e.getMessage());
+//        }
+
+
+        //try {
+//            appiumDriver.findElements(By.id("Search")).get(0).click();
+
+        appiumDriver.findElement(By.xpath("//android.widget.ImageView[@content-desc=\"Search\"]")).click();
+//        appiumDriver.findElements(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.FrameLayout/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.EditText\n"))
+//                    .get(0).sendKeys("MyDialog"+ Keys.ENTER);
+        appiumDriver.findElement(By.id("com.android.vending:id/search_box_text_input")).click();
+        appiumDriver.findElement(By.xpath("//android.widget.ImageView[@content-desc=\"Search\"]")).click();
 
 
 
-        appiumDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        //} catch (NullPointerException e) {
+            //System.out.println(e.getMessage());
+        //}
+    }
+
+    @AfterTest
+    public void End() {
+//        appiumDriver.quit();
     }
 
 }
